29a1a2c4a7d3de62d36d5c29bcff79e61f5c88bf
Pedro Carlucci <pkcarlucci@gmail.com>
2020-06-08 00:11:41 -0300

    Implementação leitor

--- Leitor.java
+++ Leitor.java
@@ -1,6 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public  class Leitor
-{
-    
+public  class Leitor{
+    File arq;
+    public Leitor(File arq){
+        this.arq = arq;
+    }
+    public Map<String, Double> read(){
+        try(BufferedReader leitor = new BufferedReader(new FileReader(arq))){
+            String linha, label, aux;
+            String[] dot;
+            String dot1;
+            int count = 0;
+            Map<String, Double> bib = new HashMap<String, Double>();
+            while((linha = leitor.readLine()) != null){
+                if(count == 0){
+                    count = 1;
+                    continue;
+                }
+                aux = linha.replaceFirst(" ", "");
+                aux = aux.replaceFirst(" ", "");
+                dot = aux.split(" ");
+                dot1 = dot[0] + " " + dot[1] + " " + dot[2];
+                label = dot[3];
+                label = label.replace("[", "");
+                label = label.replace("=", "");
+                label = label.replaceAll("([a-z])", "");
+                label = label.replaceFirst("]", "");
+                double d = Double.parseDouble(label);
+                bib.put(dot1, d);
+            }
+            return bib;
+        }
+        catch(IOException e){
+            return null;
+        }
+    }
 }
--- PrincipalLinkedList.java
+++ PrincipalLinkedList.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +8,11 @@ public class PrincipalLinkedList
     {
         Map<String, Vertice> vertices = new HashMap<String, Vertice>();
         Map<String, Aresta> arestas = new HashMap<String, Aresta>();
+        Map<String, Double> teste = new HashMap<String, Double>();
+        File arq = new File("grafo.dot");
+        Leitor testes = new Leitor(arq);
+        teste = testes.read();
+        System.out.println(teste);
         //codigo
     }
 }
